88664f2e0bf768948a7476d87357b805cbeca1df
Enrique Nuñez <enrique.programador@gmail.com>
2020-07-16 12:43:36 +0200

    row selectable, json saved

--- src/app/services/gnommes.service.ts
+++ src/app/services/gnommes.service.ts
@@ -6,10 +6,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class GnommesService {
-
+  
   private GNOMME_DATA = 'https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   public fetchData(): Observable<any> {
     return this.httpClient.get(this.GNOMME_DATA);
