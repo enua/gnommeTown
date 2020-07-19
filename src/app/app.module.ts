3663800c8d4a3132dcb1a03533e621cd5cd45a1f
Enrique Nuñez <enrique.programador@gmail.com>
2020-07-19 09:37:08 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { HomeComponent } from './home/home.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { MatCardModule } from '@angular/material/card';
     MatToolbarModule,
     MatGridListModule,
     MatCardModule,
+    MatMenuModule,
     BrowserAnimationsModule
   ],
   providers: [],
